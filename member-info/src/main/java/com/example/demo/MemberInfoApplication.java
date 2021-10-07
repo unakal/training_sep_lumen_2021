61af790883e619f7464442c635afdb83e6fc3a15
sarayu <sarayupaladugu149@gmail.com>
2021-10-07 15:16:59 +0530

    REQ-901

---
+++ member-info/src/main/java/com/example/demo/MemberInfoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MemberInfoApplication {
+
+	public static void main(String[] args) {
+		
+	ConfigurableApplicationContext ctx=	SpringApplication.run(MemberInfoApplication.class, args);
+	ctx.close();
+	}
+
+}
