bbaab8fc3ffca86e91103cb096f612556fa1386a
sarayu <sarayupaladugu149@gmail.com>
2021-10-07 12:37:41 +0530

    REQ-104 created bookController

---
+++ book-service/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
+
+
+@RestController
+public class BookController {
+
+	@Autowired
+	private BookService service;
+	
+	@GetMapping(path = "book/id")
+	public Book getBookById(@PathVariable("id") int id) {
+		return this.service.getBookById(id);
+	}
+}
