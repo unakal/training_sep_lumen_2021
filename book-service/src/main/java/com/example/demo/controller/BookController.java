c849f3ff8d47f99a42b5e60d83b2b75ea5fa52c6
sarayu <sarayupaladugu149@gmail.com>
2021-10-07 12:44:32 +0530

    REQ-106

--- book-service/src/main/java/com/example/demo/controller/BookController.java
+++ book-service/src/main/java/com/example/demo/controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Book;
@@ -10,6 +11,7 @@ import com.example.demo.service.BookService;
 
 
 @RestController
+@RequestMapping(path = "/api/v1")
 public class BookController {
 
 	@Autowired
